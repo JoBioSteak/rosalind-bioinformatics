0001a1d01e795990f093e3a7330e70ae617e7272
JoBioSteak <rennejosiah25@gmail.com>
2025-11-25 11:27:56 -0500

    Folder Location changes

--- Transcribing DNA into RNA/solution.py
+++
@@ -1,4 +0,0 @@
-file = open("C:\\Users\\derel\\Desktop\\Coding_Projects\\rosalind-bioinformatics\\Transcribing DNA into RNA\\rosalind_rna.txt")
-RNA_strand = file.read()
-print(RNA_strand.replace('T','U'))
-
