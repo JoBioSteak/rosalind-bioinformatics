6563aa82974c0ee0d8acd0f6488f328a94f8d88c
JoBioSteak <rennejosiah25@gmail.com>
2025-11-27 20:59:52 -0500

    Counting Nucleotides with BioPython

---
+++ Software-Tools_Armory/Counting DNA nucleotides using BioPython/solution.py
@@ -0,0 +1,5 @@
+from Bio.Seq import Seq
+filename = "Software-Tools_Armory\\Counting DNA nucleotides using BioPython\\rosalind_ini.txt"
+with open(filename) as f:
+    my_seq = Seq(f.read())
+    print(my_seq.count("A"),my_seq.count("C"),my_seq.count("G"),my_seq.count("T"))
