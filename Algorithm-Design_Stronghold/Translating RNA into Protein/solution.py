3e6a9dfd59bab11a9117d45ed315658d329fc3b5
JoBioSteak <rennejosiah25@gmail.com>
2025-12-02 21:47:43 -0500

    Translating RNA into Protein

---
+++ Algorithm-Design_Stronghold/Translating RNA into Protein/solution.py
@@ -0,0 +1,37 @@
+codon_table = {
+"UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "UAU": "Y", "UAC": "Y", "UAA": "Stop", "UAG": "Stop",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "UGU": "C", "UGC": "C", "UGA": "Stop", "UGG": "W",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+}
+
+file_name = "Algorithm-Design_Stronghold\\Translating RNA into Protein\\rosalind_prot.txt"
+with open(file_name) as f:
+    data = f.read()
+
+n = 3
+RNA = [(data[i:i+n]) for i in range(0,len(data),n)]
+
+protein_string = ''
+for x in range(0,len(RNA)):
+    codon = RNA[x]
+    if codon in codon_table:
+        amino_acid = codon_table[codon]
+    if amino_acid == 'Stop':
+        break
+    protein_string += amino_acid
+
+
+print(protein_string)
