e958322eaacf664093d43d08aecf7e5ed14dbcc6
JoBioSteak <rennejosiah25@gmail.com>
2025-12-05 11:28:59 -0500

    Finding a Motif in DNA

---
+++ Algorithm-Design_Stronghold/Finding a Motif in DNA/solution.py
@@ -0,0 +1,17 @@
+filename = "Algorithm-Design_Stronghold\\Finding a Motif in DNA\\rosalind_subs.txt"
+with open(filename) as f:
+    lines = f.read().split()
+
+string = lines[0]
+substring = lines[1]
+
+index = 0
+indices = []
+while index < len(string):
+    index = string.find(substring,index)
+    if index == -1:
+        break
+    indices.append(str(index+1))
+    index += 1
+
+print(" ".join(indices))
